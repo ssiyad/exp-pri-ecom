1d1df358306a0daa8c760a546ba7b2d84fde4317
Sabu Siyad <hello@ssiyad.com>
2022-07-06 01:04:53 +0530

    use in-built `express.json()`
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import bodyParser from "body-parser";
 import express from "express";
 import authRouter from "./auth";
 import buyerRouter from "./buyer";
 import _catch from "./middlewares/catch";
 
 const app = express();
-app.use(bodyParser.json())
+app.use(express.json())
 app.use('/api/auth', authRouter);
 app.use('/api/buyer', buyerRouter);
 app.use(_catch)
